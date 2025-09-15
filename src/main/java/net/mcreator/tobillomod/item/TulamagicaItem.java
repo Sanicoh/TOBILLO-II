66987323006942b818db78c72f060bb4a7dfd53b
calcetio <vjuan@mipc>
2025-09-15 20:38:57 +0200

    tulamagica

---
+++ src/main/java/net/mcreator/tobillomod/item/TulamagicaItem.java
@@ -0,0 +1,10 @@
+package net.mcreator.tobillomod.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class TulamagicaItem extends Item {
+	public TulamagicaItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
